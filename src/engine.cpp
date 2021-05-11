2fba1662376869f0f9da167437b2de3071785168
kumarharsh2396 <kumarharsh2396@gmail.com>
2021-05-11 12:15:50 +0530

    declared Randomengine class

--- src/engine.cpp
+++ src/engine.cpp
@@ -1,3 +1,42 @@
-#include "engine.h"
 
-// Create definitions of the RandomEngine class here
+#include "engine.h"
+#include <iostream>
+#include <string>
+// definitions of the RandomEngine class here
+
+// Constructors:
+RandomEngine::RandomEngine(uint8_t l){
+    
+}
+
+RandomEngine::RandomEngine(bool s){
+   
+    
+}
+
+RandomEngine::RandomEngine(uint8_t l, bool s){
+    
+}
+
+//  functions
+void RandomEngine::setLength(uint8_t l){
+    length = l;
+}
+
+uint8_t RandomEngine::getLength(){
+    return length;
+}
+
+void RandomEngine::setSymbolSet(bool s){
+    symbl = s;
+}
+
+bool RandomEngine::getSymbolSetStatus(){
+    return symbl;
+}
+
+std::string RandomEngine::getString(){
+    return "";
+
+    
+}
--- src/engine.h
+++ src/engine.h
@@ -1,4 +1,26 @@
 // Define this class here.
+
+#include <stdint.h>
+#include <string>
+
 class RandomEngine {
+    
+    public:
+        uint8_t length =0;
+        bool symbl =false;
+        std::string password = "";
+    
+    // Constructors:
+        RandomEngine(uint8_t l); 
+        RandomEngine(bool s); 
+        RandomEngine(uint8_t l, bool s); 
+
+    // functions:
+        void setLength(uint8_t);
+        void setSymbolSet(bool);
+        std::string getString(); 
+        uint8_t getLength();  
+        bool getSymbolSetStatus();  
+
 
 };
